b9e4d26e983525b85bb5721560341a55aa7715d3
Xidrovo <xidrovo@hotmail.com>
2016-10-25 23:59:57 -0500

    asd
    asdsada

---
+++ TallerUnity/Assets/Scripts/salto.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class salto : MonoBehaviour {
+
+    public static bool isGrounded = true;
+
+    void OnTriggerEnter2D(Collider2D trigg)
+    {
+        if (trigg.gameObject.tag == "gnd")
+        {
+            isGrounded = true;
+            Debug.Log("im in");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D trigg)
+    {
+        if (trigg.gameObject.tag == "gnd")
+        { 
+            isGrounded = false;
+            Debug.Log("im exit");
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
