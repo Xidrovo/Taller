4eaebb7b90bdf5548c6f22f13040c19d4f2e4ef8
Xidrovo <xidrovo@hotmail.com>
2016-10-25 23:51:11 -0500

    hola

--- TallerUnity/Assets/Scripts/Movement.cs
+++ TallerUnity/Assets/Scripts/Movement.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿  using UnityEngine;
 using System.Collections;
 
 public class Movement : MonoBehaviour {
@@ -12,11 +12,11 @@ public class Movement : MonoBehaviour {
 //		cuerpo = this.GetComponent<Rigidbody2D>();
 
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void FixedUpdate () {
 		movimientos ();
-		salto ();
+		jump ();
 	}
 
 	private void movimientos()
@@ -24,20 +24,22 @@ public class Movement : MonoBehaviour {
 		if (Input.GetKey (KeyCode.RightArrow)) 
 		{
 			//transform.Translate(speed * Time.deltaTime,0,0);
-			transform.Translate(Vector3.right * speed * Time.deltaTime);
-		}	
+			transform.Translate(Vector3.right * -speed * Time.deltaTime);
+            this.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }	
 
 		if (Input.GetKey (KeyCode.LeftArrow)) 
 		{
 			transform.Translate(Vector3.left * speed * Time.deltaTime);
-		}	
+            this.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }	
 	}
 
-	private void salto()
+	private void jump()
 	{
-		if (Input.GetKey (KeyCode.UpArrow))
+		if (Input.GetKey (KeyCode.UpArrow) && salto.isGrounded)
 		{
-			cuerpo.AddForce(Vector2.up, ForceMode2D.Impulse);
-		}
+            cuerpo.velocity = new Vector2(0,impulso);
+        }
 	}
 }
